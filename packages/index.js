594faf1bb5e9dab9bdb1801489c4a5ff6d191ee3
yang-xianzhu <92789120+yang-xianzhu@users.noreply.github.com>
2024-09-02 16:53:40 +0800

    feat: update

--- packages/index.js
+++ packages/index.js
@@ -3,6 +3,7 @@ import inquirer from 'inquirer'
 import download from 'download-git-repo'
 import chalk from 'chalk'
 import ora from 'ora'
+import fs from 'fs'
 // import { program } from 'commander'
 
 inquirer
@@ -19,16 +20,52 @@ inquirer
             choices: ['vue', 'react'],
         },
     ])
-    .then((answers) => {
+    .then(async (answers) => {
         const { tarName, template } = answers
-        const remote = `github:yang-xianzhu/atom-cl#templates/${template}`
-        const spinner = ora('download template......').start()
-        console.log(remote);
-        download(remote, tarName, { clone: true }, function (err) {
-            if (err) {
-                spinner.fail()
-                return
+        const remote = `github:yang-xianzhu/atom-cli#templates/${template}`
+
+        // 检测该目录下，有没有同名文件，有则询问用户是否覆盖
+        if (fs.existsSync(tarName)) {
+
+            const { overwrite } = await inquirer.prompt([
+                {
+                    type: 'confirm',
+                    message: '目录已存在，是否覆盖？',
+                    name: 'overwrite',
+                },
+            ]);
+
+            if (!overwrite) {
+                return;
             }
-            spinner.succeed()
+            const spinner = ora('download template......').start()
+
+            downloadTemplate(remote, tarName).then(() => {
+                spinner.succeed('download success')
+            }).catch(() => {
+                spinner.fail('download error')
+            })
+            return
+        }
+
+        const spinner = ora('download template......').start()
+
+        // 没有同名文件则直接下载
+        downloadTemplate(remote, tarName).then(() => {
+            spinner.succeed('download success')
+        }).catch(() => {
+            spinner.fail('download error')
         })
     });
+
+
+function downloadTemplate(remote, tarName) {
+    return new Promise((resolve, reject) => {
+        download(remote, tarName, {}, function (err) {
+            if (err) {
+                reject(err)
+            }
+            resolve()
+        })
+    })
+}

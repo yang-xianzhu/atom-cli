f75de9cfcb36b95cdd3bbf8e674d30f434f67154
yang-xianzhu <92789120+yang-xianzhu@users.noreply.github.com>
2024-09-02 11:34:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# A atom-cli
---
+++ packages/index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer'
+import download from 'download-git-repo'
+import chalk from 'chalk'
+import ora from 'ora'
+// import { program } from 'commander'
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: '请输入项目名称',
+            name: 'tarName',
+        },
+        {
+            type: 'list',
+            message: '请选择项目模板',
+            name: 'template',
+            choices: ['vue', 'react'],
+        },
+    ])
+    .then((answers) => {
+        const { tarName } = answers
+        const remote = 'https://github.com:johanazhu/koa-basic#master'
+        const spinner = ora('download template......').start()
+        download(remote, tarName, { clone: true }, function (err) {
+            if (err) {
+                spinner.fail()
+                return
+            }
+            spinner.succeed()
+        })
+    });
